cbd80b6994a024d63dbf462d191c441b364fe899
Cranky Bawler <6666lenia6666@mail.ru>
2025-02-06 18:14:43 +0300

    1

---
+++ src/main/java/pro/sky/CoursePaperJavaCore/CoursePaperJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CoursePaperJavaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaperJavaCoreApplication.class, args);
+	}
+
+}
